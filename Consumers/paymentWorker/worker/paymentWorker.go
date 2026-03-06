f377c32484d355430b14d5900e8efebe46e485a3
KabeerJamal <kabeerjamal7@gmail.com>
2026-03-06 22:18:57 +0100

    converting message.value to PaymentSuccessMessage struct

--- Consumers/paymentWorker/worker/paymentWorker.go
+++ Consumers/paymentWorker/worker/paymentWorker.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,9 +12,13 @@ import (
 
 func PaymentWorker(ctx context.Context) error {
 
-	//read from kafka
-
 	kafkaBrokerAddress := os.Getenv("KAFKA_BROKER_HOST_docker")
+
+	// w := kafka.NewWriter(kafka.WriterConfig{
+	// 	Brokers:  []string{kafkaBrokerAddress},
+	// 	Topic:    "Payment-Successful",
+	// 	Balancer: &kafka.Hash{},
+	// })
 
 	// 1. Create reader config
 	r := kafka.NewReader(kafka.ReaderConfig{
@@ -37,8 +42,57 @@ func PaymentWorker(ctx context.Context) error {
 
 		// 3. Print message
 		fmt.Printf("Received message: key=%s value=%s\n", string(msg.Key), string(msg.Value))
-		//if success, update db status to paid
+		//if success, send topic to insertion worker to update database
+		var paymentSuccessMessage PaymentSuccessMessage
+
+		//call a function which takes in msg.Value and returns paymentMessage
+		paymentSuccessMessage, err = convertToPaymentMessage(msg.Value)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		fmt.Print(paymentSuccessMessage.Status)
+		return nil
+		// if paymentMessage.Status == "completed" {
+		// 	valueBytes, err := json.Marshal(paymentMessage)
+		// 	if err != nil {
+		// 		log.Fatal(err)
+		// 		return err
+		// 	}
+
+		// 	w.WriteMessages(
+		// 		ctx,
+		// 		kafka.Message{
+		// 			Key:   msg.Key,
+		// 			Value: valueBytes,
+		// 		},
+		// 	)
+		// }
+
+		//do idempotency check
+		//update redis too
 		//if failed, write to kafka, which will be read by undo worker
+		//upadte redis too
 	}
 
+}
+
+type PaymentSuccessMessage struct {
+	TicketUUID string `json:"TicketUUID"`
+	PhoneUUID  string `json:"PhoneUUID"`
+	UserUUID   string `json:"UserUUID"`
+	Status     string `json:"Status"`
+}
+
+// When you use json.Unmarshal into a struct, extra fields in the JSON are ignored automatically.
+// So if msg.Value contains many more fields, Go will just fill the ones that match your struct and skip the rest
+func convertToPaymentMessage(data []byte) (PaymentSuccessMessage, error) {
+	var pm PaymentSuccessMessage
+
+	err := json.Unmarshal(data, &pm)
+	if err != nil {
+		return PaymentSuccessMessage{}, err
+	}
+
+	return pm, nil
 }
